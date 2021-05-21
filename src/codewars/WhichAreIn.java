ac1787eb0c796d2ddf08272207261873e9139cf6
zouhaire <zouhaire@idside.de>
2021-05-21 10:54:58 +0100

    added WhichArein class

---
+++ src/codewars/WhichAreIn.java
@@ -0,0 +1,25 @@
+package codewars;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class WhichAreIn {
+
+    public static String[] inArray(String[] array1, String[] array2){
+
+        return Arrays.stream(array1)
+              .filter( s -> Arrays.stream(array2).anyMatch( value -> value.contains(s)))
+              .distinct()
+              .sorted()
+              .toArray(String[]::new);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+                WhichAreIn.inArray(
+                        new String[]{"test1", "test2"},
+                        new String[]{"atest1", "btest2"})
+                        .length
+        );
+    }
+}
